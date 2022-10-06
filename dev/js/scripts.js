57a66c5c0767eda16e07f23690a80b42a3ad904e
NaimSaleem <102745638+NaimSaleem@users.noreply.github.com>
2022-10-06 05:11:46 -0400

    GSAP Animations added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,2 +1,6 @@
 import { gsap } from "gsap";
 
+
+gsap.from('#first-line', {duration: 2, x: -100, alpha: 0});
+gsap.from('#second-line', {duration: 2, x: 100, alpha: 0, delay: 2});
+gsap.from('button', {duration: 1, y: '-100%', alpha:0, ease: 'bounce', delay: 4});
