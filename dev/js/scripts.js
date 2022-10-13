d1a3f7c93025c906fad688ae7d8913052f453ce6
NaimSaleem <102745638+NaimSaleem@users.noreply.github.com>
2022-10-13 11:02:44 -0400

    Added Timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,56 @@
 import { gsap } from "gsap";
 
 
-gsap.from('#first-line', {duration: 2, x: -100, alpha: 0});
-gsap.from('#second-line', {duration: 2, x: 100, alpha: 0, delay: 2});
-gsap.from('button', {duration: 1, y: '-100%', alpha:0, ease: 'bounce', delay: 4});
+function heroAnimation(){
+    var tl = gsap.timeline();
+    tl.from('#first-line', {duration: 0.5, x: -100, alpha: 0})
+        .from('#second-line', {duration: 0.5, x: 100, alpha: 0})
+        .from('button', {duration: 0.5, y: '-100%', alpha:0, ease: 'bounce'})
+        .from('button i', {duration: 0.5, y: '-100%', alpha:0 });
+    return tl;
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(heroAnimation());
+
+var heroSizeNumber = 1;
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 768px)", () => {
+
+    heroSizeNumber = 2;
+
+});
+
+mm.add("(max-width: 767px)", () => {
+
+    heroSizeNumber = 1.25;   
+
+});
+
+let trailBtn = document.querySelector('button');
+
+
+
+var buttonAnimation = gsap.timeline({paused:true});
+trailBtn.addEventListener("mouseover",function(){
+    buttonAnimation.play()
+    buttonAnimation.to("button",{duration:0.4, scale:heroSizeNumber})
+        .to('#button i',{duration: 0.25, rotateY:180})
+        .to("#first-line",{duration: 0.25, alpha:0, y:50})
+        .to("#second-line",{duration: 0.25, alpha:0, y:20});
+});
+
+trailBtn.addEventListener("mouseout",function(){
+    buttonAnimation.reverse();
+    buttonAnimation.to('button',{duration:0.4, scale:1})
+
+    .to('button i',{duration: 0.25, rotateY:0})
+
+    .to("#first-line",{duration: 0.25, alpha:1, y:0})
+    .to("#second-line",{duration: 0.25, alpha:1, y:0});
+});
+
  
