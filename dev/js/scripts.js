3377529ea69ec7a6f7ec0ce5fa9b2e09f7673559
NaimSaleem <102745638+NaimSaleem@users.noreply.github.com>
2022-10-16 20:39:17 -0400

    Moved Josh's files to my project

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,54 +1,29 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 
-function heroAnimation(){
+function scrollAnimation(){
     var tl = gsap.timeline();
-    tl.from('#first-line', {duration: 0.5, x: -100, alpha: 0})
-        .from('#second-line', {duration: 0.5, x: 100, alpha: 0})
-        .from('button', {duration: 0.5, y: '-100%', alpha:0, ease: 'bounce'})
-        .from('button i', {duration: 0.5, y: '-100%', alpha:0 });
+    tl.to("#box", {
+        duration: 1,
+        x: 400,
+        rotation: 360,
+        scrollTrigger: {
+            trigger: "#box",
+            // toggleActions: "restart pause reverse pause",
+            markers:true,
+            start: "0px 50%",
+            end: "100% 10%",
+            scrub: true,
+            pin: true
+        }
+    });
     return tl;
 }
 
 
 var mainTL = gsap.timeline();
-mainTL.add(heroAnimation());
+mainTL.add(scrollAnimation());
 
-var heroSizeNumber = 1;
-
-let mm = gsap.matchMedia();
-
-mm.add("(min-width: 768px)", () => {
-
-    heroSizeNumber = 1.2;
-
-});
-
-mm.add("(max-width: 767px)", () => {
-
-    heroSizeNumber = 1.1;   
-
-});
-
-let trailBtn = document.querySelector('button');
-
-
-
-var buttonAnimation = gsap.timeline({paused:true});
-trailBtn.addEventListener("mouseover",function(){
-    buttonAnimation.play()
-    buttonAnimation.to("button",{duration:0.2, scale:heroSizeNumber})
-        .to('button i',{duration: 0.25, rotateX:180})
-        .to("#first-line",{duration: 0.25, alpha:0, y:50})
-        .to("#second-line",{duration: 0.25, alpha:0, y:20});
-});
-
-trailBtn.addEventListener("mouseout",function(){
-    buttonAnimation.reverse();
-    buttonAnimation.to("#first-line",{duration: 0.25, alpha:0, y:0})
-    .to("#second-line",{duration: 0.25, alpha:0, y:0})
-    .to('button i',{duration: 0.25, rotateX:0})
-    .to('button',{duration:0.2, scale:heroSizeNumber});
-});
-
- 
