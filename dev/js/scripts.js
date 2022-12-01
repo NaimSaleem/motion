18905b3144b1cce85f3376d3a6c59976036975b9
NaimSaleem <102745638+NaimSaleem@users.noreply.github.com>
2022-12-01 11:50:31 -0500

    Added animations and everything
    Re-added things because I lost all of my progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,142 +3,115 @@ import { gsap } from "gsap";
 
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { CustomEase } from "gsap/CustomEase";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MotionPathPlugin, CustomEase);
 
-function lineMotion(){
+// var wiggles = 10; //tweak this to whatever number you want. 
+
+// //create the custom eases..
+// CustomWiggle.create("Wiggle.easeOut", {wiggles:wiggles, type:"easeOut"});
+// CustomWiggle.create("Wiggle.easeInOut", {wiggles:wiggles, type:"easeInOut"});
+// CustomWiggle.create("Wiggle.anticipate", {wiggles:wiggles, type:"anticipate"});
+// CustomWiggle.create("Wiggle.uniform", {wiggles:wiggles, type:"uniform"});
+// CustomWiggle.create("Wiggle.random", {wiggles:wiggles, type:"random"});
+
+function ahoyAnimation(){
     var tl = gsap.timeline();
-
-    tl.from("#Vector1",{
-        duration: 2,
-        drawSVG: "40 % 50%",
-        stroke: "white",
-        scale: 2
+    tl.to("#ahoylogo",{
+        duration: 0.002,
+        x: 400, 
+        rotate: 90,
+        opacity: 0,
+        scale: 2,
+        transformOrigin: "center"
     })
-    tl.from("#Vector2",{
-        duration: 2,
-        drawSVG: 0
-    })
-
-    return tl;
-}
-
-
-function patternMotion(){
-    var tl = gsap.timeline();
-
-        tl.from(".red",{
-            duration: 0.5  ,
-            drawSVG: "50% 50%",
-            stroke: "white",
-            strokeWidth: 30,
-            rotate: 360,
-            transformOrigin: "center",
-            stagger: 0.1,
-            scale: 2
-
-        })
-        .from(".orange",{
-            duration: 0.5, 
-            drawSVG: "50% 50%",
-            stroke: "white",
-            strokeWidth: 30,
-            rotate: 360,
-            transformOrigin: "center",
-            stagger: 0.1,
-            scale: 2
-        })
-        .to(".red",{
-            duration: 0.2,
-            fill: "red",
-            stagger: 0.01
-            
-        })
-        .to(".orange",{
-            duration: 0.2,
-            fill: "orange",
-            stagger: 0.01
-
-        })
-    return tl;
-}
-
-
-function UIMotion(){
-    var tl = gsap.timeline()
-        tl.from("#Ellipse-1",{
-            transformOrigin: "center",
-            duration: 2,
-            drawSVG: 0,
-            scale: 2
-
-        })
-       
-        tl.from("#Polygon-1",{
-            duration: 0.5,
-            drawSVG: 0
-
-        },"triangle")
+    tl.from("#ahoylogo",{
+        duration: 0.02,
+        opacity: 0
         
-        tl.from("#Ellipse-2",{
-            duration: 1.5,
-            x: 100,
-            delay: 1,
+        
+
+    })
+    tl.from("#Vector1",{
+        duration: 1,
+        drawSVG: 0
+        
+    })
+    tl.to("#Vector1",{
+        duration: 0.9,
+        rotate: -90,
+        y: 200,
+        scale: 0.5,
+        transformOrigin: "center"
+    },"lettermove")
+
+        tl.from(".letters",{
+            duration: 0.6,
+            stagger: 0.05,
+            x: "+=50",
             opacity: 0
-            
-        },"mouseHover")
-        tl.from(".sparkle",{
-            duration: 0.1,
-            opacity: 0,
-            stroke: 0
+
+        },"lettermove")
+        tl.from(".letters",{
+            duration: 1,
+            scale: 2
+        },"lettermove")
+
+        
+        tl.to("#Rectangle_1",{
+            delay: 2,
+            duration: 1,
+            x: "+=1400"
+        },"colorchange")
+
+        tl.to("#Rectangle_2",{
+            delay: 2,
+            duration: 1,
+            x: "+=1400"
+        },"colorchange2")
+
+        tl.to("#Rectangle_3",{
+            delay: 2,
+            duration: 1,
+            x: "+=1400"
+        },"colorchange3")
+
+        tl.to(".letters",{
+            delay: 2,
+            fill: "#24323E",
+            stagger: 0.02
+        },"colorchange")
+
+        tl.to(".letters",{
+            delay:2,
+            fill: "#FFFFFF",
+            stagger: 0.02
+        },"colorchange3")
+        
+        tl.to("#Vector1",{
+            delay: 2,
+            stroke: "#24323E"
+        },"colorchange2")
+
+        tl.to("#Vector1",{
+            delay: 2,
+            stroke: "#E0B562"
+        },"colorchange3")
+
+        tl.to(".letters",{
+            duration: 4,
+            opacity: 100
         })
-        tl.to(".sparkle",{
-            duration: 0.4,
-            opacity: 0,
-            scale: 1,
-            stroke: 0,
-            rotate: -45,
-            fill: "orange"
 
-        },"sparkleLeave")
-        tl.to("#Polygon-1",{
-            duration: 0.2,
-            fill: "white"
-        },"sparkleLeave")
-        tl.to("#Ellipse-1",{
-            duration: 0.2,
-            fill: "red"
-        },"sparkleLeave")
-        tl.to("#Polygon-1",{
-            transformOrigin: "center",
-            duration: 0.2,
-            drawSVG: "50% 50%"
-        },"sparkleLeave")
-        tl.to("#Ellipse-1",{
-            transformOrigin: "center",
-            scale: 1.2
-        },"sparkleLeave")
-        tl.to("#Polygon-1",{
-            transformOrigin: "center",
-            scale: 1.2
-        },"sparkleLeave")
-        tl.to("#Ellipse-1",{
-            transformOrigin: "center",
-            duration: 0.2,
-            drawSVG: "50% 50%"
-        },"sparkleLeave")
-        tl.to("#Ellipse-2",{
-            duration:0.5,
-            x: 200,
-            delay: 1,
-            opacity: 0
-
-        }) 
     return tl;
 }
+
+
+
 var mainTL = gsap.timeline();
-mainTL.add(lineMotion())
-.add(patternMotion())
-.add(UIMotion());
+mainTL.add(ahoyAnimation());
+
 
 GSDevTools.create();
